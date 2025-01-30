cb78592939da87fb6140163490c20d4dd7aed357
rafaelrc7 <contact@rafaelrc.com>
2025-01-30 18:55:30 -0300

    Refactor reader

--- app/bg-rd/Main.hs
+++ app/bg-rd/Main.hs
@@ -1,26 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Main where
 
-import           Control.Monad    (unless)
-import           Data.List        (sort)
-import           Data.Text        (Text)
-import qualified Data.Text        as T
-import qualified Data.Text.IO     as T
-import           Settings         (Reference (..), Settings (..), parseCLIArgs)
-import           System.Directory (doesDirectoryExist, listDirectory)
-import           System.Exit      (exitFailure)
-import           System.IO        (hPutStrLn, stderr)
+import           Control.Monad                 (unless, when)
+import           Data.List                     (sort)
+import           Data.Maybe                    (fromMaybe)
+import           Data.Text                     (Text)
+import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as T
+import           Prettyprinter                 (Doc, annotate, fill, indent,
+                                                line, pretty, vsep, (<+>))
+import           Prettyprinter.Render.Terminal (AnsiStyle, bold, putDoc,
+                                                underlined)
+import           Settings                      (Book, Chapter, Reference (..),
+                                                Settings (..), Verse,
+                                                parseCLIArgs)
+import           System.Directory              (doesDirectoryExist,
+                                                listDirectory)
+import           System.Exit                   (exitFailure)
+import           System.FilePath               (dropTrailingPathSeparator,
+                                                takeBaseName, (</>))
+import           System.IO                     (hPutStrLn, stderr)
+import           System.IO.Error               (isDoesNotExistError, tryIOError)
 
-data Error = InvalidBook Text
-           | InvalidChapter Text Text
-           | InvalidVerse Text Text Text
+data Error = InvalidBook Book
+           | InvalidBookChapter Book Chapter
+           | InvalidChapterVerse Chapter Verse
+           | InvalidBookChapterVerse Book Chapter Verse
   deriving (Show)
 
 main :: IO ()
 main = do
   settings <- parseCLIArgs
-  let bibleDir = baseDir settings ++ "/" ++ T.unpack (bibleVersion settings)
+  let bibleDir = baseDir settings </> T.unpack (bibleVersion settings)
 
   doesDirectoryExist bibleDir >>= \dirExists -> unless dirExists $ do
     hPutStrLn stderr $ "Selected bible '" ++ bibleDir ++ "' does not exist"
@@ -29,184 +42,203 @@ main = do
   ret <- mapM (getReferece bibleDir) (bibleReference settings)
 
   case sequence ret of
-    Right txts -> mapM_ T.putStrLn txts
+    Right txts -> putDoc $ prettyBooks txts
     Left err -> do
       case err of
         InvalidBook book ->
           T.hPutStrLn stderr $ "Selected book '" <> book <> "' does not exist at '" <> T.pack bibleDir <> "'"
-        InvalidChapter book chapter ->
-          T.hPutStrLn stderr $ "Selected chapter '" <> chapter <> "' does not exist in book '"
-            <> book <> "' at '" <> T.pack bibleDir <> "'"
-        InvalidVerse book chapter verse ->
-          T.hPutStrLn stderr $ "Selected verse '" <> verse <> "' does not exist in book '"
-            <> book <> ":" <> chapter <> "' at '" <> T.pack bibleDir <> "'"
+        InvalidBookChapter book chapter ->
+          T.hPutStrLn stderr $ "Selected chapter '" <> book <> ":" <> T.pack (show chapter) <> "' does not exist at '" <> T.pack bibleDir <> "'"
+        InvalidChapterVerse chapter verse ->
+          T.hPutStrLn stderr $ "Selected verse '" <> T.pack (show chapter) <> ":" <> T.pack (show verse) <> "' does not exist at '" <> T.pack bibleDir <> "'"
+        InvalidBookChapterVerse book chapter verse ->
+          T.hPutStrLn stderr $ "Selected verse '" <> book <> ":" <> T.pack (show chapter) <> ":" <> T.pack (show verse) <> "' does not exist at '" <> T.pack bibleDir <> "'"
       exitFailure
 
-getReferece :: FilePath -> Reference -> IO (Either Error Text)
+getReferece :: FilePath -> Reference -> IO (Either Error (Book, [((Chapter, Verse), Text)]))
 getReferece biblePath (Book book) = do
   path' <- getBookPath biblePath book
   case path' of
     Left err -> return $ Left err
     Right path -> do
-      chapters <- getBookChapters path
-      let chaptersPaths = map (\chapter -> path ++ "/" ++ chapter) chapters
-      chaptersTexts <- mapM T.readFile chaptersPaths
-      let chaptersTexts' = addBookName book $ T.unlines $ addChapterNumbers $ map addVerseNumbers chaptersTexts
-      return $ Right chaptersTexts'
+      chapters <- getChapterList path
+      text <- getBook path chapters
+      case text of
+        Left err    -> return $ Left err
+        Right text' -> return $ Right (book, text')
 getReferece biblePath (BookChapter book chapter) = do
   path' <- getBookPath biblePath book
   case path' of
     Left err -> return $ Left err
     Right path -> do
-      chapters <- getBookChapters path
-      let chapter' = show chapter
-      if chapter' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter')
-      else do
-        let chapterPath = path ++ "/" ++ chapter'
-        chapterText <- T.readFile chapterPath
-        let chapterText' = addBookName book $ addChapterNumber chapter $ addVerseNumbers chapterText
-        return $ Right chapterText'
+      text <- getBook path [chapter]
+      case text of
+        Left err    -> return $ Left err
+        Right text' -> return $ Right (book, text')
 getReferece biblePath (BookChapterRange book chapter1 chapter2) = do
   path' <- getBookPath biblePath book
   case path' of
     Left err -> return $ Left err
     Right path -> do
-      chapters <- getBookChapters path
-      let chapter1' = show chapter1
-      let chapter2' = show chapter2
-      if chapter1' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter1')
-      else if chapter2' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter2')
-      else do
-        let selectedChapters = [chapter1..chapter2]
-        let chaptersPaths = map (\chapter -> path ++ "/" ++ show chapter) selectedChapters
-        chaptersTexts <- mapM T.readFile chaptersPaths
-        let chaptersTexts' = addBookName book $ T.unlines $ addChapterNumbersFrom chapter1 $ map addVerseNumbers chaptersTexts
-        return $ Right chaptersTexts'
+      chapters <- getChapterList path
+      let selectedChapters = getChapterRange (length chapters) (Just chapter1) (Just chapter2)
+      text <- getBook path selectedChapters
+      case text of
+        Left err    -> return $ Left err
+        Right text' -> return $ Right (book, text')
 getReferece biblePath (BookChapterVerses book chapter verses) = do
   path' <- getBookPath biblePath book
   case path' of
     Left err -> return $ Left err
     Right path -> do
-      chapters <- getBookChapters path
-      let chapter' = show chapter
-      if chapter' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter')
-      else do
-        let chapterPath = path ++ "/" ++ chapter'
-        chapterText <- T.readFile chapterPath
-        let chapterVerses = T.lines chapterText
-        let chapterSize = length chapterVerses
-        let verses' = mapM (\verse ->
-                          if verse < 1 || verse > chapterSize then
-                            Left (InvalidVerse book (T.pack $ show chapter) (T.pack $ show verse))
-                          else
-                            Right (verse, chapterVerses !! (verse-1)))
-                        verses
-        case verses' of
-          Left err         -> return $ Left err
-          Right versesText -> return $ Right $ addBookName book $ addChapterNumber chapter $ T.unlines $ addVersesNumbers versesText
+      let verses' = map (chapter, ) verses
+      text <- getBook path [chapter]
+      case text of
+        Left err -> return $ Left err
+        Right text' -> do
+          let text'' = filterVerses verses' text'
+          case text'' of
+            Left (InvalidChapterVerse c v) -> return $ Left (InvalidBookChapterVerse book c v)
+            Left err                       -> return $ Left err
+            Right text'''                  -> return $ Right (book, text''')
 getReferece biblePath (BookChapterVerseRange book chapter verse1 verse2) = do
   path' <- getBookPath biblePath book
   case path' of
     Left err -> return $ Left err
     Right path -> do
-      chapters <- getBookChapters path
-      let chapter' = show chapter
-      if chapter' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter')
-      else do
-        let chapterPath = path ++ "/" ++ chapter'
-        chapterText <- T.readFile chapterPath
-        let chapterVerses = T.lines chapterText
-        let chapterSize = length chapterVerses
-        if verse1 < 1 || verse1 > chapterSize then
-          return $ Left (InvalidVerse book (T.pack $ show chapter) (T.pack $ show verse1))
-        else if verse2 < 1 || verse2 > chapterSize then
-          return $ Left (InvalidVerse book (T.pack $ show chapter) (T.pack $ show verse2))
-        else do
-          let versesText = T.unlines $ take (verse2-verse1+1) $ drop (verse1-1) chapterVerses
-          let versesText' = addBookName book $ addChapterNumber chapter $ addVerseNumbersFrom verse1 versesText
-          return $ Right versesText'
+      text <- getBook path [chapter]
+      case text of
+        Left err -> return $ Left err
+        Right text' -> do
+          let text'' = filterVerseRange (Just (chapter, verse1)) (Just (chapter, verse2)) text'
+          case text'' of
+            Left (InvalidChapterVerse c v) -> return $ Left (InvalidBookChapterVerse book c v)
+            Left err                       -> return $ Left err
+            Right text'''                  -> return $ Right (book, text''')
 getReferece biblePath (BookChaptersVerseRange book chapter1 verse1 chapter2 verse2) = do
   path' <- getBookPath biblePath book
   case path' of
     Left err -> return $ Left err
     Right path -> do
-      chapters <- getBookChapters path
-      let chapter1' = show chapter1
-      let chapter2' = show chapter2
-      if chapter1' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter1')
-      else if chapter2' `notElem` chapters then
-        return $ Left $ InvalidChapter book (T.pack chapter2')
-      else do
-        let selectedChapters = [chapter1..chapter2]
-        let chaptersPaths = map (\chapter -> path ++ "/" ++ show chapter) selectedChapters
-        chaptersTexts <- mapM T.readFile chaptersPaths
-        let chaptersTextsSize = length chaptersTexts
-        if null chaptersTexts then
-          return $ Left $ InvalidChapter book (T.pack chapter1')
-        else do
-          let chapter1Verses = T.lines $ head chaptersTexts
-          let chapter2Verses = T.lines $ last chaptersTexts
-          let chapter1Size = length chapter1Verses
-          let chapter2Size = length chapter2Verses
-          if verse1 < 1 || verse1 > chapter1Size then
-            return $ Left (InvalidVerse book (T.pack $ show chapter1) (T.pack $ show verse1))
-          else if verse2 < 1 || verse2 > chapter2Size then
-            return $ Left (InvalidVerse book (T.pack $ show chapter2) (T.pack $ show verse2))
-          else do
-            if chapter1 /= chapter2 then do
-              let chapter1Verses' = addVerseNumbersFrom verse1 $ T.unlines $ drop (verse1-1) chapter1Verses
-              let chapter2Verses' = addVerseNumbers $ T.unlines $ take verse2     chapter2Verses
-              let remainingChapters = map addVerseNumbers $ take (chaptersTextsSize-2) $ drop 1 chaptersTexts
-              let selectedVerses = chapter1Verses' : remainingChapters ++ [chapter2Verses']
-              let selectedVerses' = addBookName book $ T.unlines $ addChapterNumbersFrom chapter1 selectedVerses
-              return $ Right selectedVerses'
-            else do
-              let versesText = T.unlines $ take (verse2-verse1+1) $ drop (verse1-1) chapter1Verses
-              let versesText' = addBookName book $ addChapterNumber chapter1 $ addVerseNumbersFrom verse1 versesText
-              return $ Right versesText'
+      chapters <- getChapterList path
+      let selectedChapters = getChapterRange (length chapters) (Just chapter1) (Just chapter2)
+      text <- getBook path selectedChapters
+      case text of
+        Left err    -> return $ Left err
+        Right text' -> do
+          case filterVerseRange (Just (chapter1, verse1)) (Just (chapter2, verse2)) text' of
+            Left (InvalidChapterVerse c v) -> return $ Left (InvalidBookChapterVerse book c v)
+            Left err                       -> return $ Left err
+            Right text''                   -> return $ Right (book, text'')
+
+----
+
+getBookList :: FilePath -> IO [FilePath]
+getBookList = listDirectory
 
 getBookPath :: FilePath -> Text -> IO (Either Error FilePath)
 getBookPath biblePath book = do
-  let path = biblePath ++ "/" ++ T.unpack book
+  let path = biblePath </> T.unpack book
   exists <- doesDirectoryExist path
   if exists then
     return $ Right path
   else
-    return $ Left $ InvalidBook book
+    return $ Left (InvalidBook book)
 
-getBookChapters :: FilePath -> IO [FilePath]
-getBookChapters bookPath = do
-  verses <- listDirectory bookPath
-  return $ map show $ sort $ map (read :: FilePath -> Int) verses
+----
 
-addVerseNumbers :: Text -> Text
-addVerseNumbers = addVerseNumbersFrom 1
+getChapterList :: FilePath -> IO [Int]
+getChapterList bookPath = sort . map read <$> listDirectory bookPath
 
-addChapterNumbers :: [Text] -> [Text]
-addChapterNumbers = addChapterNumbersFrom 1
+getChapter :: FilePath -> Int -> IO (Either Error [(Int, Text)])
+getChapter bookPath chapter = do
+  let book = takeBaseName $ dropTrailingPathSeparator bookPath
+  let chapterPath = bookPath </> show chapter
+  verses <- tryIOError $ T.readFile chapterPath
+  case verses of
+    Right verses'                       -> return $ Right $ zip [1..] $ T.lines verses'
+    Left err
+      | isDoesNotExistError err -> return $ Left $ InvalidBookChapter (T.pack book) chapter
+      | otherwise               -> ioError err
 
-addVerseNumbersFrom :: Int -> Text -> Text
-addVerseNumbersFrom start chapterText = T.unlines . addVersesNumbers $ verses'
-  where verses = T.lines chapterText
-        verses' = zip [start..] verses
+getChapters :: FilePath -> [Int] -> IO (Either Error [(Int, [(Int, Text)])])
+getChapters bookPath chapters = fmap (zip chapters) . sequence <$> mapM (getChapter bookPath) chapters
 
-addVersesNumbers :: [(Int, Text)] -> [Text]
-addVersesNumbers = map (\(n, t) -> T.pack (show n) <> ". " <> t)
+getBook :: FilePath -> [Int] -> IO (Either Error [((Int, Int), Text)])
+getBook  bookPath chapters = fmap joinChapterVerseNumber <$> getChapters bookPath chapters
 
-addChapterNumbersFrom :: Int -> [Text] -> [Text]
-addChapterNumbersFrom start chapters = chapters''
-  where chapters' = zip [start..] chapters
-        chapters'' = map (uncurry addChapterNumber) chapters'
+joinChapterVerseNumber :: [(Int, [(Int, Text)])] -> [((Int, Int), Text)]
+joinChapterVerseNumber = concatMap (uncurry addChapterNumber')
+  where addChapterNumber' :: Int -> [(Int, Text)] -> [((Int, Int), Text)]
+        addChapterNumber' c = map (\(v, t) -> ((c, v), t))
 
-addChapterNumber :: Int -> Text -> Text
-addChapterNumber chapter chapterText = "Chapter " <> T.pack (show chapter) <> "\n" <> chapterText
+----
 
-addBookName :: Text -> Text -> Text
-addBookName book text = "Book of " <> book <> "\n\n" <> text
+getChapterRange :: Int -> Maybe Int -> Maybe Int -> [Int]
+getChapterRange chapterNum from to = [fromMaybe 1 from..fromMaybe chapterNum to]
+
+filterVerses :: [(Int, Int)] -> [((Int, Int), Text)] -> Either Error [((Int, Int), Text)]
+filterVerses []           _  = Right []
+filterVerses ((sc, sv):_) [] = Left (InvalidChapterVerse sc sv)
+filterVerses (sv:svs)     vs = do
+  (selv, vs') <- filterVerse sv vs
+  selvs       <- filterVerses svs vs'
+  return (selv : selvs)
+
+filterVerse :: (Int, Int) -> [((Int, Int), Text)] -> Either Error (((Int, Int), Text), [((Int, Int), Text)])
+filterVerse (sc, sv) [] = Left (InvalidChapterVerse sc sv)
+filterVerse s@(sc, sv) (v@(m@(mc, mv), _):vs) = do
+  when (mc > sc || mc == sc && mv > sv) $ Left (InvalidChapterVerse sc sv)
+  if s == m then
+    Right (v, vs)
+  else
+    filterVerse s vs
+
+filterVerseRange :: Maybe (Int, Int) -> Maybe (Int, Int) -> [((Int, Int), Text)] -> Either Error [((Int, Int), Text)]
+filterVerseRange from to verses = do
+  verses' <- case from of
+    Just from' -> filterVersesFrom from' verses
+    Nothing    -> Right verses
+
+  case to of
+    Just to' -> filterVersesTo to' verses'
+    Nothing  -> Right verses'
+
+filterVersesTo :: (Int, Int) -> [((Int, Int), Text)] -> Either Error [((Int, Int), Text)]
+filterVersesTo (toc, tov) [] = Left (InvalidChapterVerse toc tov)
+filterVersesTo to@(toc, tov) (v@(vr@(vrc, _), _):vs)
+  | vr == to  = Right [v]
+  | vrc > toc = Left (InvalidChapterVerse toc tov)
+  | otherwise = (v :) <$> filterVersesTo to vs
+
+filterVersesFrom :: (Int, Int) -> [((Int, Int), Text)] -> Either Error [((Int, Int), Text)]
+filterVersesFrom (fromc, fromv)      [] = Left (InvalidChapterVerse fromc fromv)
+filterVersesFrom from@(fromc, fromv) vs'@((vr@(vrc, _), _):vs)
+  | vr == from  = Right vs'
+  | vrc > fromc = Left (InvalidChapterVerse fromc fromv)
+  | otherwise = filterVersesFrom from vs
+
+----
+
+prettyVerses :: [((Int, Int), Text)] -> Doc AnsiStyle
+prettyVerses verses = vsep markedVerses
+  where showVerseMark :: (Int, Int) -> Text
+        showVerseMark (c, v) = T.pack (show c) <> ":" <> T.pack (show v)
+
+        getMaxVerseMarkWidth :: [Text] -> Int
+        getMaxVerseMarkWidth vs = maximum $ map T.length vs
+
+        (verseMarks, verseTexts) = unzip verses
+        verseMarks' = map showVerseMark verseMarks
+        verseMarks'' = map (indent 2 . annotate bold . fill verseMarkWidth . pretty . showVerseMark) verseMarks
+        verseMarkWidth = getMaxVerseMarkWidth verseMarks' + 1
+        markedVerses = zipWith (<+>) verseMarks'' (map pretty verseTexts)
+
+prettyBook :: Text -> [((Int, Int), Text)] -> Doc AnsiStyle
+prettyBook book verses = book' <> line <> line <> verses'
+  where book' = annotate (bold <> underlined) $ "Book of" <+> pretty book
+        verses' = prettyVerses verses
+
+prettyBooks :: [(Text, [((Int, Int), Text)])] -> Doc AnsiStyle
+prettyBooks books = vsep $ map ((<> line) . uncurry prettyBook) books
 
